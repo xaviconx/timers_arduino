38c6ca33d7ad934d097295c2582b9d09dbf299f0
Pedro Alexandro Osorio Gonzalez <alex@MacBook-Pro-de-Pedro.local>
2022-10-08 19:10:28 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//Este código está hecho para funcionar en Arduino UNO.
+//Su función será correr 3 timers que inicien al mismo tiempo y mostrar el orden en que fueron
+//activados en un LCD de 16x2
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
